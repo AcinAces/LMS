51c1073400c518b455db421b8bd3bba95a66d12e
Al Irfan Alve <114176932+AcinAces@users.noreply.github.com>
2026-08-26 03:11:44 +0600

    deployment issue fix

--- backend/src/middlewares/force-https.ts
+++ backend/src/middlewares/force-https.ts
@@ -1,7 +1,9 @@
 export default () => {
   return async (ctx: any, next: any) => {
     if (process.env.NODE_ENV === 'production') {
-      ctx.request.header['x-forwarded-proto'] = 'https';
+      if (ctx.cookies) {
+        ctx.cookies.secure = true;
+      }
     }
     await next();
   };
